5a61accc5e515b098642cf21894f2d88e435d545
feels15man <ilya.shabanov2004@mail.ru>
2020-11-23 18:59:06 +0300

    2

--- main.py
+++ main.py
@@ -1,6 +1,43 @@
-def main():
-    print('My first git program')
+from PyQt5.QtWidgets import QWidget, QApplication, QMainWindow, QLabel, QGridLayout
+from PyQt5.QtGui import QPainter, QPixmap, QPen, QColor
+from PyQt5.QtCore import Qt
+from PyQt5 import uic
+from random import randint
+
+
+class Test(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('untitled.ui', self)
+
+        self.pushButton.setFixedSize(100, 100)
+        self.pushButton.clicked.connect(self.draw)
+
+        self.label = QLabel()
+        canvas = QPixmap(600, 600)
+        self.label.setPixmap(canvas)
+
+        layout = QGridLayout(self.centralwidget)
+        layout.addWidget(self.pushButton, 0, 0, alignment=Qt.AlignCenter)
+        layout.addWidget(self.label, 1, 0)
+
+    def draw(self):
+        x, y = [randint(10, 500) for i in range(2)]
+        w, h = [randint(10, 100) for i in range(2)]
+        # создаем экземпляр QPainter, передавая холст (self.label.pixmap())
+        painter = QPainter(self.label.pixmap())
+        pen = QPen()
+        pen.setWidth(3)
+        pen.setColor(QColor(*[randint(0, 255) for i in range(3)]))
+        painter.setPen(pen)
+        painter.drawEllipse(x, y, w, h)
+        painter.end()
+        self.update()
 
 
 if __name__ == '__main__':
-    main()
+    import sys
+    app = QApplication(sys.argv)
+    ex = Test()
+    ex.show()
+    sys.exit(app.exec_())
