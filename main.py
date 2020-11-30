fb2a3a1d0665a0bae95f8a3dd71e7816698701d5
feels15man <ilya.shabanov2004@mail.ru>
2020-11-30 18:47:54 +0300

    lgbt circles

--- main.py
+++ main.py
@@ -3,30 +3,32 @@ from PyQt5.QtGui import QPainter, QPixmap, QPen, QColor
 from PyQt5.QtCore import Qt
 from PyQt5 import uic
 from random import randint
+from untitled import Ui_MainWindow
 
 
 class Test(QMainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi('untitled.ui', self)
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
 
-        self.pushButton.setFixedSize(100, 100)
-        self.pushButton.clicked.connect(self.draw)
+        self.ui.pushButton.setFixedSize(100, 100)
+        self.ui.pushButton.clicked.connect(self.draw)
 
-        self.label = QLabel()
+        self.ui.label = QLabel()
         canvas = QPixmap(600, 600)
-        self.label.setPixmap(canvas)
+        self.ui.label.setPixmap(canvas)
 
-        layout = QGridLayout(self.centralwidget)
-        layout.addWidget(self.pushButton, 0, 0, alignment=Qt.AlignCenter)
-        layout.addWidget(self.label, 1, 0)
+        self.ui.layout = QGridLayout(self.ui.centralwidget)
+        self.ui.layout.addWidget(self.ui.pushButton, 0, 0, alignment=Qt.AlignCenter)
+        self.ui.layout.addWidget(self.ui.label, 1, 0)
 
     def draw(self):
         v = randint(10, 100)
         x, y = [randint(10, 500) for i in range(2)]
         w, h = v, v
         # создаем экземпляр QPainter, передавая холст (self.label.pixmap())
-        painter = QPainter(self.label.pixmap())
+        painter = QPainter(self.ui.label.pixmap())
         pen = QPen()
         pen.setWidth(3)
         pen.setColor(QColor(*[randint(0, 255) for _ in range(3)]))
